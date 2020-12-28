6eaaa514bb6c77b5e8eeee2a5fac9b96e2eae3ce
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-28 17:36:51 +0300

    Calculate state prediction variance

--- state_prediction.cpp
+++ state_prediction.cpp
@@ -9,7 +9,7 @@ double new_mean, new_var;
 tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2)
 {
     new_mean = mean1 + mean2; //State prediction mean function mu;
-    new_var =  //TODO: Code the state prediction variance function sigma square;
+    new_var =  var1 + var2; //State prediction variance function sigma square;
     return make_tuple(new_mean, new_var);
 }
 
