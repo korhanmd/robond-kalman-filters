301059f545793bf1ed9cb66237a6f3b5d780df41
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-28 14:56:58 +0300

    Calculate measurement update variance

--- measurement_update.cpp
+++ measurement_update.cpp
@@ -9,7 +9,7 @@ double new_mean, new_var;
 tuple<double, double> measurement_update(double mean1, double var1, double mean2, double var2)
 {
     new_mean = (var2*mean1 + var1*mean2)/(var1 + var2); // Measurment update mean function mu;
-    new_var =  //TODO: Code the measurment update variance function sigma square;
+    new_var =  1/((1/var2) + (1/var1)); // Measurment update variance function sigma square;
     return make_tuple(new_mean, new_var);
 }
 
