4a30b61f7f1666ab3db0d552e77884831540fb1c
Korhan Mutludoğan <korhanmd@gmail.com>
2021-01-03 20:30:13 +0300

    Initialize S

--- kalman_multidimensional.cpp
+++ kalman_multidimensional.cpp
@@ -24,7 +24,7 @@ tuple<MatrixXf, MatrixXf> kalman_filter(MatrixXf x, MatrixXf P, MatrixXf u, Matr
         MatrixXf y(1, 1);
         y << Z - (H*x);
         
-        
+        MatrixXf S(1, 1);
         
         
         // Prediction
